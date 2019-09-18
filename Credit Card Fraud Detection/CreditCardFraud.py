f5b6fee01241bbf8664ed76bed7b5a9833097947
Chandan <chandan.kum.panda@gmail.com>
2019-09-18 17:25:35 +0530

    Credit Card Fraud CLeaning Done.

--- Credit Card Fraud Detection/CreditCardFraud.py
+++ Credit Card Fraud Detection/CreditCardFraud.py
@@ -1,29 +1,31 @@
-"""
-Data Set Download Link
-https://www.kaggle.com/mlg-ulb/creditcardfraud/downloads/creditcardfraud.zip/3
-"""
+#Importing Libraries
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split as TTS
+from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.metrics import confusion_matrix as CM
+from sklearn.metrics import roc_auc_score as RAS
 
-
-
+#Declaring Variables and Values
 LABELS = ["Normal", "Fraud"]
+TRAIN_SIZE=0.20
+TEST_SIZE=0.20
+RANDOM_STATE=2000
+NUMBER_OF_JOBS=4
+N_ESTIMATORS=100
+CMAP="RdYlGn"
+CRITERION='gini'
 
 
 data = pd.read_csv('creditcard.csv')
 data.head()
 
-
-
 data.info()
 
-
-
 data.isnull().values.any()
-
-
 
 count_classes = pd.value_counts(data['Class'])
 count_classes.plot(kind = 'bar', rot=0)
@@ -32,30 +34,24 @@ plt.xticks(range(2), LABELS)
 plt.xlabel("Normal / Fraud  Class")
 plt.ylabel("Number of Transactions")
 
+"""The Data is highly UNBALANCED"""
 
-## Get the Fraud and the normal dataset 
+# Get the Fraud and the normal dataset 
 
 fraud = data[data['Class']==1]
 
 normal = data[data['Class']==0]
 
-
-
 print(fraud.shape,normal.shape)
 
-
-
-## We need to analyze more amount of information from the transaction data
+##analyze more amount of information from the transaction data
 #How different are the amount of money used in different transaction classes?
 fraud.Amount.describe()
 
-
-
 normal.Amount.describe()
 
-
-
-
+"""We see that the mean of transaction for FRAUD is higher that the NORMAL trnsaction"""
+"""Lest see the transaction amount vs FRAUD and NORMAL Transaction"""
 f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 f.suptitle('Amount per transaction by class')
 bins = 50
@@ -69,10 +65,12 @@ plt.xlim((0, 20000))
 plt.yscale('log')
 plt.show();
 
+"""WE see that the Fraud transaction occured for smaller values , around 50$"""
 
 
 
-# We Will check Do fraudulent transactions occur more often during certain time frame ? Let us find out with a visual representation.
+# We Will check Do fraudulent transactions occur more often during certain time frame ? 
+#Let us find out with a visual representation.
 
 f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 f.suptitle('Time of transaction vs Amount by class')
@@ -84,16 +82,15 @@ plt.xlabel('Time (in Seconds)')
 plt.ylabel('Amount')
 plt.show()
 
+"""We se that the Fraud is occoured during the whole time frame.
+Its not concentrated to just one time frame."""
 
 #get correlations of each features in dataset
-corrmat = data.corr()
-top_corr_features = corrmat.index
+correlationData = data.corr()
+top_corr_features = correlationData.index
 plt.figure(figsize=(20,20))
 #plot heat map
-g=sns.heatmap(data[top_corr_features].corr(),annot=True,cmap="RdYlGn")
-
-
-
+g=sns.heatmap(data[top_corr_features].corr(),annot=True,cmap=CMAP)
 
 var = data.columns.values
 
@@ -113,7 +110,7 @@ for feature in var:
     plt.xlabel(feature, fontsize=12)
     locs, labels = plt.xticks()
     plt.tick_params(axis='both', which='major', labelsize=12)
-plt.show();
+plt.show()
 
 
 # 
@@ -127,53 +124,40 @@ plt.show();
 
 
 
-target  = 'Class'
-predictor = ['Time', 'V1', 'V2', 'V3', 
-     'V4', 'V5', 'V6', 'V7', 
-     'V8', 'V9', 'V10','V11', 
-     'V12', 'V13', 'V14', 'V15', 
-     'V16', 'V17', 'V18', 'V19',
-     'V20', 'V21', 'V22', 'V23', 
-     'V24', 'V25', 'V26', 'V27',
-     'V28', 'Amount']
+RESULT  = 'Class'
+RESULT_DEPENDENT = ['Time', 'V1', 'V2', 'V3', 
+                    'V4', 'V5', 'V6', 'V7', 
+                    'V8', 'V9', 'V10','V11', 
+                    'V12', 'V13', 'V14', 'V15', 
+                    'V16', 'V17', 'V18', 'V19',
+                    'V20', 'V21', 'V22', 'V23', 
+                    'V24', 'V25', 'V26', 'V27',
+                    'V28', 'Amount']
 
 
 
 ## Split data in train, test and validation set
-from sklearn.model_selection import train_test_split
 
-data_train, data_test = train_test_split(data, test_size=0.20, random_state=2000,shuffle=True )
-data_train, data_valid = train_test_split(data_train, test_size=0.20, random_state=2000,shuffle=True)
+
+data_train, data_test = TTS(data, test_size=TRAIN_SIZE, random_state=RANDOM_STATE,shuffle=True )
+data_train, data_valid = TTS(data_train, test_size=TEST_SIZE, random_state=RANDOM_STATE,shuffle=True)
 
 
 
 
 #Random Forest Classifier
-from sklearn.ensemble import RandomForestClassifier as RFC
-rfc = RFC(n_jobs=4, 
-          random_state=2000,
-          criterion='gini',
-          n_estimators=100,
+
+rfc = RFC(n_jobs=NUMBER_OF_JOBS, 
+          random_state=RANDOM_STATE,
+          criterion=CRITERION,
+          n_estimators=N_ESTIMATORS,
           verbose=False)
 
+rfc.fit(data_train[RESULT_DEPENDENT], data_train[RESULT].values)
 
+predictRFC = rfc.predict(data_valid[RESULT_DEPENDENT])
 
+cmRFC = CM(data_valid[RESULT].values, predictRFC)
 
-rfc.fit(data_train[predictor], data_train[target].values)
-
-
-
-
-predsRFC = rfc.predict(data_valid[predictor])
-
-
-
-
-from sklearn.metrics import confusion_matrix
-cmRFC = confusion_matrix(data_valid[target].values, predsRFC)
-
-cmRFC
-
-from sklearn.metrics import roc_auc_score
-accuracyRFC=roc_auc_score(data_valid[target].values, predsRFC)
+accuracyRFC=RAS(data_valid[RESULT].values, predictRFC)
 print("The accuracy of Random Forest Classification is: " + str(accuracyRFC*100)+"%")
